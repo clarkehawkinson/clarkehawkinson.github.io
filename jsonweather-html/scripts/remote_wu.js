a36e67a465bf8f51cd1da95717c7610fb6a24bd4
clarkehawkinson <provojuggler@gmail.com>
2017-06-24 14:05:55 -0600

    major update

---
+++ jsonweather-html/scripts/remote_wu.js
@@ -0,0 +1,46 @@
+// Current Location Scripts
+$(function () {
+
+  var status = $('#status');
+
+  (function getGeoLocation() {
+    status.text('Getting Location...');
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        var lat = position.coords.latitude;
+        var long = position.coords.longitude;
+
+        // Call the getData function, send the lat and long
+        getData(lat, long);
+
+      });
+    } else {
+      status.text("Your browser doesn't support Geolocation or it is not enabled!");
+    }
+
+  })();
+
+  // Get the data from the wunderground API
+  function getData(lat, long){
+    $.ajax({
+       
+url:"http://api.wunderground.com/api/b58ad7db802544af/geolookup/conditions/q/"+lat+","+long+".json",
+dataType : "jsonp",
+  success : function(parsed_json) {
+  var location = parsed_json['location']['city'];
+  var temp_f = parsed_json['current_observation']['temp_f'];
+  var state = parsed_json['location']['state'];
+
+$("#currentTemp").html(temp_f);
+$("#cityDisplay").html(location+","+state);
+$("#cover").fadeOut(250);
+    }
+    });
+
+  }
+
+  // A function for changing a string to TitleCase
+  function toTitleCase(str){
+    return str.replace(/\w+/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  }
+});
