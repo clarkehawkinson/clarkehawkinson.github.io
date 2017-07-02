fd8288f29135755d297a962398e8cc8ae7a979ec
clarkehawkinson <provojuggler@gmail.com>
2017-07-01 19:29:17 -0600

    big update

--- weathersearch.js
+++ weathersearch.js
@@ -1,29 +1,26 @@
 var returned;
-
 function getData(input) {
-    // Get the data from the wunderground API
-    $.ajax({
-        url: "https://api.wunderground.com/api/b58ad7db802544af/geolookup/forecast/conditions/q/" +
-            input + ".json",
-        dataType: "jsonp",
-        success: function (data) {
-            console.log(data);
-            var location = data.location.city + ', ' + data.location.state;
-            var temp_f = data.current_observation.temp_f;
-            var dew = data.current_observation.dewpoint_f;
-            var tempHigh = data.forecast.simpleforecast.forecastday[1].high.fahrenheit;
-            console.log(data.forecast.simpleforecast.high);
-            var tempLow = data.forecast.simpleforecast.forecastday[1].low.fahrenheit;
-            console.log('Location is: ' + location);
-            console.log('Temp is: ' + temp_f);
-            $("#title").html(location + " | Weather Center");
-            $("#currentTemp").html(Math.round(temp_f) + '°');
-            $("#add1").text(toTitleCase(data.current_observation.weather));
-            $("#add2").html(tempHigh + "°F / " + tempLow + '°F')
-            $("#").fadeOut(250);
-        }
-    });
-};
+  // Get the data from the wunderground API
+  $.ajax({
+    url: "https://api.wunderground.com/api/b58ad7db802544af/geolookup/forecast/conditions/q/"
+    + input + ".json"
+    , dataType: "jsonp"
+    , success: function (parsed_json) {
+      console.log(data);
+      var location = data.location.city + ', ' + data.location.state;
+      var temp_f = data.current_observation.temp_f;
+      var dew = data.current_observation.dewpoint_f;
+      console.log('Location is: ' + location);
+      console.log('Temp is: ' + temp_f);
+      
+      $("#cityDisplay").text(location);
+      $("#title").text(location + " | Weather Center");
+      $("#currentTemp").text(Math.round(temp_f) + '°');
+      $("#add1").text(toTitleCase(data.current_observation.icon));
+      $("#add2").text(dew); 
+  }});
+}
+;
 
 $('#query').keyup(function () {
     var value = $('#query').val();
@@ -46,22 +43,22 @@ $('#query').keyup(function () {
 });
 // Intercept the menu link clicks
 $("#searchResults").on("click", "a", function (evt) {
-    evt.preventDefault();
-    // With the text value get the needed value from the weather.json file
-    var jsonCity = $(this).text(); // Franklin, etc...
-    console.log(jsonCity);
+  evt.preventDefault();
+  // With the text value get the needed value from the weather.json file
+  var jsonCity = $(this).text(); // Franklin, etc...
+  console.log(jsonCity);
     index = $(this).index("a");
     getData(returned.RESULTS[index].name);
     //hide search results
     document.getElementById('searchResults').style.display = 'none';
 
-    // Get weather data from wunderground.com
+// Get weather data from wunderground.com
 
-});
+  });
 
-function toTitleCase(str) {
-    return str.replace(/\w+/g, function (txt) {
-        return txt.charAt(0).toUpperCase() +
+function toTitleCase(str){
+    return str.replace(/\w+/g, function (txt){
+        return txt.charAt(0).toUpperCase()+
             txt.substr(1).toLowerCase();
     })
 }
@@ -76,3 +73,4 @@ function toTitleCase(str) {
 //    //hide search results
 //    document.getElementById('searchResults').style.display = 'none';
 //});
+
